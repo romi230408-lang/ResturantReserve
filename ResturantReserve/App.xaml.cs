a28e603d3f6993a94096e104e755f94d31189b75
User <User@T12>
2001-10-10 16:49:07 +0200

    Add project files.

---
+++ ResturantReserve/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ResturantReserve
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ResturantReserve/ViewModels/LoginPageVM.cs
@@ -0,0 +1,58 @@
+﻿using ResturantReserve.Models;
+using System.Windows.Input;
+using Microsoft.Maui.Storage;
+
+
+namespace ResturantReserve.ViewModels
+{
+    internal partial class LoginPageVM : ObservableObject
+    {
+        private User user = new();
+        public ICommand LoginCommand { get; set; }
+
+        public string Email
+        {
+            get => user.Email;
+            set
+            {
+                if (user.Email != value)
+                {
+                    user.Email = value;
+                    (LoginCommand as Command)?.ChangeCanExecute();
+                }
+            }
+
+        }
+
+        public string Password
+        {
+            get => user.Password;
+            set
+            {
+                if (user.Password != value)
+                {
+                    user.Password = value;
+                    (LoginCommand as Command)?.ChangeCanExecute();
+                }
+            }
+        }
+
+        public bool CanLogIn()
+        {
+            return !string.IsNullOrWhiteSpace(Email) &&
+                   !string.IsNullOrWhiteSpace(Password);
+        }
+
+        public LoginPageVM()
+        {
+            LoginCommand = new Command(LogIn);
+        }
+
+        private void LogIn()
+        {
+            user.Login();
+        }
+
+    }
+
+}
